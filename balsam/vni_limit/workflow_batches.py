31aaa0390d4434597ac56450d88f9fb74269949d
Christine Simpson <csimpson@aurora-uan-0010.hostmgmt.cm.aurora.alcf.anl.gov>
2025-06-04 17:17:37 +0000

    fixed balsam vni limit example

--- balsam/vni_limit/workflow_batches.py
+++ balsam/vni_limit/workflow_batches.py
@@ -5,7 +5,7 @@ from balsam.api import ApplicationDefinition,Job,BatchJob,Site
 # when mpiexec calls return
 
 class hello_sleep(ApplicationDefinition):
-    site = "aurora-testing"
+    site = "aurora_testing"
 
     command_template = "sleep {{sleep_time}} && echo 'sleep for {{sleep_time}} seconds'"
 
@@ -26,31 +26,34 @@ for i in range(num_batches):
     batch_tag = f"batch_{i}"
     for j in range(batch_size):
         if len(jobs) < ntasks:
-            job = Job.objects.create( app_id="hello_sleep",
-                        site_name="aurora-testing",
+            job = Job( app_id="hello_sleep",
+                        site_name="aurora_testing",
                         workdir=f"{batch_tag}/task_{j}",
                         gpus_per_rank=1,
                         ranks_per_node=12,
                         node_packing_count=1,
                         num_nodes=2,
-                        params={"sleep_time": 10},
+                        parameters={"sleep_time": 10},
                         tags={"batch": batch_tag},)
             jobs.append(job)
 
 jobs = Job.objects.bulk_create(jobs)
+for job in jobs:
+    if type(job) is not Job:
+        print(job)
+print(f"Added {len(jobs)} to the site")
 
 # Submit batch jobs for each batch
-site = Site.objects.get("aurora-testing")
+site = Site.objects.get("aurora_testing")
 
 for i in range(num_batches):
     batch_tag = f"batch_{i}"
     BatchJob.objects.create(site_id=site.id,
                        num_nodes=32,
-                       wall_time_min=10,
+                       wall_time_min=60,
                        job_mode="mpi",
                        filter_tags={"batch":batch_tag},
                        project="datascience",
                        queue="prod",
     )
-
-
+print(f"Submitted batch jobs")
